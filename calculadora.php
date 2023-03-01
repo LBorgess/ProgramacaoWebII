7e2ea745525a7f9a8dd03bbe90e79697da0758e2
LBorgess <lucas_rancer@hotmail.com>
2023-03-01 20:16:55 -0300

    calculadora simple

---
+++ calculadora.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	$v1 = 30;
+	$v2 = 15;
+
+	echo "Calculadora <br/>";
+
+	$soma = sprintf("A soma é: %d", $v1 + $v2);
+	echo $soma;
+	// printf("%d + %d = %d", $v1, $v2, $v1 + $v2);
+
+?>
