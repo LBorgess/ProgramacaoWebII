8e51dc956aed812c9e9c5d0735a27a3e1075af1d
LBorgess <lucas_rancer@hotmail.com>
2023-03-01 19:26:41 -0300

    tipo de variaveis

---
+++ variavel.php
@@ -0,0 +1,13 @@
+<?php 
+
+$data = "20 de agostode 2020"; // string
+$salario = 850.00;             // float ou double
+$cargo = "Estagiario";         // string
+$idade = 18;                   // interger ou int
+$resultado = true;             // boolean
+
+if($resultado){
+	echo "Verdadeiro";
+}
+
+?>
