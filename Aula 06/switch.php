c77c12d130956e7046deb702d55e86b9dbae43ba
LBorgess <lucas_rancer@hotmail.com>
2023-03-20 19:11:12 -0300

    comando switch-case

---
+++ Aula 06/switch.php
@@ -0,0 +1,22 @@
+<?php 
+	
+	// Similar a uma combinação de if-elseif-else
+
+	$opcao = 5;
+
+	switch ($opcao) {
+		case 1:
+			echo "A opção 1 foi selecionado";
+			break;
+		case 2:
+			echo "A opção 1 foi selecionado";
+			break;
+		case 3:
+			echo "A opção 1 foi selecionado";
+			break;
+		default:
+			echo "Opção incorreta";
+			break;
+	}
+
+?>
