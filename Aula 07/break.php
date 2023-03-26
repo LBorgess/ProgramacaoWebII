ced769e99c1b9f0c9c51618b04e3583f9311f882
LBorgess <lucas_rancer@hotmail.com>
2023-03-26 17:28:25 -0300

    Exemplo break

---
+++ Aula 07/break.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	/**
+	 * Permite finalizar a execução de um bloco.
+	 * Pode-se utilizar em blocos de repetição, switch etc
+	 */
+
+	$x = 0;
+	while($x < 100){
+		echo "<br/>" . $x;
+		if($x == 10){
+			break;
+			$x++;
+		}
+	}
+
+?>
