bf214d298bac9683708efbee8bcbd835ad90f6ee
LBorgess <lucas_rancer@hotmail.com>
2023-03-26 17:24:45 -0300

    Exemplo de foreach

---
+++ Aula 07/foreach.php
@@ -0,0 +1,13 @@
+<?php 
+	
+	/**
+	 * É uma simplificação do for para se trabalhar em coleções
+	 * de dados, ou seja, vetores e matrizes.
+	 */
+
+	$nomes = array('João', 'José', 'Paulo', 'Pedro');
+
+	foreach($nomes as $item){
+		echo "<br/>" . $item;
+	}
+?>
