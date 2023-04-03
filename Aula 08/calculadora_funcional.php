3db3dd73cf2ff3a7ac55e3172932b722a678a4d3
LBorgess <lucas_rancer@hotmail.com>
2023-04-03 20:16:58 -0300

    Função imprimir()

--- Aula 08/calculadora_funcional.php
+++ Aula 08/calculadora_funcional.php
@@ -3,49 +3,50 @@
 	# Funções
 
 	function somar($x, $y){
-		echo ("<br/>SOMA: $x + $y = ". ($x + $y));
+		return $x + $y;
 	}
 
 	function subtrai($x, $y){
-		echo ("<br/>SUBTRAÇÃO: $x - $y = ". ($x - $y));
+		return $x - $y;
 	}
 
 	function multiplica($x, $y){
-		echo ("<br/>MULTIPLICAÇÃO: $x x $y = ". ($x * $y));
+		return $x * $y;
 	}
 
 	function dividir($x, $y){
 		if($y == 0){
 			echo "<br/> IMPOSSÍVEL CALCULAR!";
 		} else{
-			$fmt = sprintf("\nDIVISÃO: %.2f / %.2f = %.2f", $x, $y, ($x / $y));
-			echo $fmt;
+			return $x / $y;
 		}
 	}
 
-
-	# ===
+	function imprimi($opcao, $operacao){
+		$fmt = sprintf("OPÇÃO: %s <br/>RESULTADO %d ", $opcao, $operacao);
+		echo $fmt;
+	}
 
 	# Variáveis
 
 	$a = 10;
-	$b = 2.6;
-	$opcao = '+';
+	$b = 20;
+	$op = '+';
 
 	# Operações;
 
-	switch ($opcao) {
+	switch ($op) {
 		case '+':
-			somar($a, $b);			
+			imprimi($op, somar($a, $b));		
 			break;
 		case '-':
-			echo(subtrai($a, $b));
+			imprimi(subtrai($a, $b));
 			break;
 		case '*':
-			echo(multiplica($a, $b));
+			imprimi(multiplica($a, $b));
 			break;
 		case '/':
-			echo(dividir($a, $b));
+			imprimi(dividir($a, $b));
 			break;		
 		default:
 			echo "SELEÇÃO INVÁLIDA.";
