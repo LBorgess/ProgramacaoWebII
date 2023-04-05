6c78fd5ecab86a4c75c0f26b013d6c5a79df77d5
LBorgess <lucas_rancer@hotmail.com>
2023-04-05 19:50:00 -0300

    Include_once

---
+++ Aula 08/include_once.php
@@ -0,0 +1,11 @@
+<?php 
+	# Inclui o cabeçalho da página apenas uma única vez
+	include_once("cabecalho.php");
+	include_once("cabecalho.php");
+	include_once("cabecalho.php");
+
+	# Inclui o rodapé da página 3 vezez
+	include("rodape.php");
+	include("rodape.php");
+	include("rodape.php");
+?>
