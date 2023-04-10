70221e6bbecc61234c32ef61b28ca1bbcff9c332
LBorgess <lucas_rancer@hotmail.com>
2023-04-10 19:27:40 -0300

    Vetor simples

---
+++ Aula 09/vetor.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<p>Exemplo declaração de váriavel sem array</p>
+
+	<?php 
+
+		$nome1 = "Maria";
+		$nome2 = "João";
+		$nome3 = "Pedro";
+		$nome4 = "Paulo";
+		$nome5 = "Lucas";
+
+		echo "$nome1<br/>$nome2<br/>$nome3<br/>$nome4<br/>$nome5<br/>";
+
+	?>
+</body>
+</html>
