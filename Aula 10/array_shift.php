c84b2ef612d04e77c25f59ed3ca5b28017ff5c9f
LBorgess <lucas_rancer@hotmail.com>
2023-04-17 19:22:38 -0300

    Função array_shift()

---
+++ Aula 10/array_shift.php
@@ -0,0 +1,12 @@
+<?php 
+
+	$nomes = array("Maria", "Jose", "Pedro");
+	$excluido = array_shift($nomes);
+
+	foreach($nomes as $item){
+		echo "<br/>".$item;
+	}
+
+	echo "<br/><br/> Nome removido: ".$excluido;
+
+?>
