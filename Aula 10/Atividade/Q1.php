5e1159c78f9cdaadd0044c9fcb4bb7c454fab734
LBorgess <lucas_rancer@hotmail.com>
2023-04-19 19:47:30 -0300

    Atividade 3 - Q1 Parcial

---
+++ Aula 10/Atividade/Q1.php
@@ -0,0 +1,26 @@
+<?php 
+
+/*
+1. Entre  com  os  dados  de  10  alunos  de  uma  classe,  recebendo  as  informações  como nome  e  uma  nota  do  aluno.  
+Armazene  estes  dados  em  um  vetor.  
+Ao  final  do programa  mostrar  a  média  de  nota  da  classe,  e  o  nome  do  aluno  que  obteve  maior nota. 
+*/
+
+	$notas_nomes = array(
+		"Gustavo"   => 10,
+		"Adriano"   => 10,
+		"Lucas"     => 10,
+		"Ryan"      => 10,
+		"Henzo"     => 10,
+		"João"      => 10,
+		"Guilherme" => 10,
+		"David"     => 10,
+		"Henrique"  => 10,
+		"Laercio"   => 10
+	);
+
+	
+
+
+
+?>
