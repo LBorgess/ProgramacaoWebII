dc836cc225157d86beb5f2519f56846640d0c343
LBorgess <lucas_rancer@hotmail.com>
2023-04-19 20:08:56 -0300

    Atividade 3 - Q1

--- Aula 10/Atividade/Q1.php
+++ Aula 10/Atividade/Q1.php
@@ -7,20 +7,40 @@ Ao  final  do programa  mostrar  a  média  de  nota  da  classe,  e  o  nome  d
 */
 
 	$notas_nomes = array(
-		"Gustavo"   => 10,
-		"Adriano"   => 10,
 		"Lucas"     => 10,
-		"Ryan"      => 10,
+		"Adriano"   => 10,
+		"Gustavo"   => 7,
+		"Ryan"      => 8,
 		"Henzo"     => 10,
 		"João"      => 10,
 		"Guilherme" => 10,
 		"David"     => 10,
 		"Henrique"  => 10,
-		"Laercio"   => 10
+		"Laércio"   => 10
 	);
 
-	
+	$notas = array_values($notas_nomes);
+	$soma_notas = 0;
 
+	for ($i = 0; $i < 10 ; $i++) { 
+		$soma_notas += $notas[$i];
+	}
+
+	$media = ($soma_notas / 10);
+
+	$maior_nota = 0;
+
+	for($i = 0; $i < 10; $i++){
+		if($maior_nota < $notas[$i]){
+			$maior_nota = $notas[$i];
+		}
+	}
+
+	$nome = array_search($maior_nota, $notas_nomes);
+
+
+	echo "<br/> MÉDIA DA CLASSE.....: " . $media;
+	echo "<br/> ALUNO COM MAIOR NOTA: " . $nome;
 
 
 ?>
