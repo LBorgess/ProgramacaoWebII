a8195f36fb539ffcf728f793f9163bb6dc1c9027
LBorgess <lucas_rancer@hotmail.com>
2023-04-26 19:28:03 -0300

    Função array_reverse()

---
+++ Aula 11/array_reverse.php
@@ -0,0 +1,11 @@
+<?php 
+
+	$nomes = array("MA" => "Maria", "ZE" => "José", "PE" => "Pedro", "AN" => "Ana");
+
+	$reverso = array_reverse($nomes);
+
+	echo "<pre>";
+	print_r($reverso);
+	echo "<pre>";
+
+?>
