d7bbac3cc6823760fadd34c9de252793859df9f4
LBorgess <lucas_rancer@hotmail.com>
2023-05-22 19:33:06 -0300

    Calculadora com formulário

---
+++ Aula 15/Atividade/calculadora.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<title>Calculadora</title>
+</head>
+<body>
+
+	<h2 class="text-center">Resultado</h2>
+
+	<?php
+
+
+		function somar($x, $y){
+			return $x + $y;
+		}
+
+		$valor1 = $_POST['num1'];
+		$valor2 = $_POST['num2'];
+
+
+		$resultado = somar($valor1, $valor2);
+
+	?>
+
+
+	<div class="container text-center p-2">
+		<div class="row justify-content-center align-items-center">
+			<div class="alert alert-primary" role="alert">
+				<?php 
+
+					echo $valor1 . " + " . $valor2 . " = " . $resultado;
+
+				?>
+			</div>
+		</div>
+	</div>
+
+	<div class="justify-content-center align-items-center row">
+		<a class="btn btn-info" href="./calculadora.html">Voltar</a>		
+	</div>
+
+
+
+
+	<!-- Bootstrap JS -->
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	
+</body>
+</html>
