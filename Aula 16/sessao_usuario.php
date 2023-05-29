b632c34ad583eb0d0fabf013ee39c7c1100b48a6
LBorgess <lucas_rancer@hotmail.com>
2023-05-29 19:29:36 -0300

    session_start()

---
+++ Aula 16/sessao_usuario.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sessão Usuário</title>
+</head>
+<body>
+	<?php
+		// Cria a sessão 
+		session_start();
+		// session_destroy(); [Elimina todas as sessões]
+		// session_unset(); [Elimina as sessão]
+
+		$_SESSION['nome'] = "Lucas";
+	?>
+
+	<h3>Sessão de Usuário</h3>
+	Bem vindo, Sr. <?php echo $_SESSION['nome']; ?>
+
+</body>
+</html>
